796e63df444715c4b4bcdbfb00186cdabccc7b77
Md. Habibur Rahman Bappy <habib.rahman@live.com>
2025-07-09 02:23:03 +0600

    initial server setup with mongodb connection, hidden credectial with env file, gitignore, basic get api / created

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xgjcv8g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Headliner API is running");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("headlinerDB");
+
+    await client.db("headLinerDB").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run().catch(console.error);
+
+app.listen(port, () => {
+  console.log(`✅ Server running on Port: ${port}`);
+});
